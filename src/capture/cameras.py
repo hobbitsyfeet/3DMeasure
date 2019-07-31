d0c193bd397351c77fbae1a74118a9890bfdf808
Justin <lego.man.123@hotmail.com>
2019-07-30 18:35:25 -0600

    Updated Camera. (Untested)

--- src/capture/cameras.py
+++ src/capture/cameras.py
@@ -52,14 +52,18 @@ save_path = "./data/"
 colorizer_1 = rs.colorizer()
 colorizer_2 = rs.colorizer()
 
-# filters = [rs.disparity_transform(),
-#                 rs.spatial_filter(),
-#                 rs.temporal_filter(),
-#                 rs.disparity_transform(False)]
+filters = [rs.disparity_transform(),
+                rs.spatial_filter(),
+                rs.temporal_filter(),
+                rs.disparity_transform(False)]
 
 def nothing(x):
     pass
-def view_pointcloud(path_1, path_2):
+
+def o3d_view_pointcloud(path_1, path_2):
+    """
+    View two pointlcouds from a their paths in one Open3D visualization.
+    """
     o3d_cloud1 = o3d.io.read_point_cloud(path_1, format="ply")
     o3d_cloud2 = o3d.io.read_point_cloud(path_2, format="ply")
     o3d.visualization.draw_geometries([o3d_cloud1, o3d_cloud2])
@@ -69,87 +73,109 @@ def view_pointcloud(path_1, path_2):
 #     pcl_cloud2 = pcl.load_XYZRGB(path_2, format="ply")
 #     viewer = pcl_visualization.CloudViewing()
 #     viewer.ShowColorCloud(pcl_cloud1)
-#     # viewer.ShowColorCloud(pcl_cloud2)
+#     viewer.ShowColorCloud(pcl_cloud2)
 #     v = True
 #     while v:
 #         v = not(viewer.WasStopped())
-    # pcl.save(cloud,"./data/Best2_Scaled_Monkey.ply",format="ply",binary=False)
-def get_depth_data(frame_1, frame_2):
-        # frames_1 = pipeline_1.wait_for_frames()
-        depth_frame_1 = frame_1.get_depth_frame()
-        color_frame_1 = frame_1.get_color_frame()
-
-        depth_frame_1 = decimate.process(depth_frame_1)
-
-        # Convert images to numpy arrays
-        depth_image_1 = np.asanyarray(depth_frame_1.get_data())
-        color_image_1 = np.asanyarray(color_frame_1.get_data())
-        colorized_depth_1 = colorizer_1.colorize(depth_frame_1)
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap_1 = np.asanyarray(colorized_depth_1.get_data())
-        # depth_colormap_1 = cv2.applyColorMap(cv2.convertScaleAbs(depth_image_1, alpha=0.5), cv2.COLORMAP_JET)
-
-        # Camera 2
-        # Wait for a coherent pair of frames: depth and color
-        frames_2 = pipeline_2.wait_for_frames()
-        depth_frame_2 = frames_2.get_depth_frame()
-        color_frame_2 = frames_2.get_color_frame()
 
 
-        depth_frame_1 = decimate.process(depth_frame_1)
-        depth_frame_2 = decimate.process(depth_frame_2)
+def get_depth_data(frame_1, frame_2, color_frame_1, color_frame_2):
+    """
+    Returns depth data ready to export. 
+    This depth data is processed and has the respective colour images mapped onto them.
+    """
+    # frames_1 = pipeline_1.wait_for_frames()
+    depth_frame_1 = frame_1.get_depth_frame()
+    # color_frame_1 = frame_1.get_color_frame()
+    depth_frame_1 = decimate.process(depth_frame_1)
 
-        # for f in filters:
-        #         depth_frame_1 = f.process(depth_frame_1)
-        #         depth_frame_2 = f.process(depth_frame_2)
+    # Convert images to numpy arrays
+    depth_image_1 = np.asanyarray(depth_frame_1.get_data())
+    color_image_1 = np.asanyarray(color_frame_1.get_data())
+    colorized_depth_1 = colorizer_1.colorize(depth_frame_1)
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap_1 = np.asanyarray(colorized_depth_1.get_data())
+    # depth_colormap_1 = cv2.applyColorMap(cv2.convertScaleAbs(depth_image_1, alpha=0.5), cv2.COLORMAP_JET)
 
-        # Convert images to numpy arrays
-        depth_image_2 = np.asanyarray(depth_frame_2.get_data())
-        color_image_2 = np.asanyarray(color_frame_2.get_data())
-        colorized_depth_2 = colorizer_2.colorize(depth_frame_2)
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap_2 = np.asanyarray(colorized_depth_2.get_data())
-        depth_colormap_2 = cv2.applyColorMap(cv2.convertScaleAbs(depth_image_2, alpha=0.5), cv2.COLORMAP_JET)
-        # Stack all images horizontally
-        mapped_frame_1, color_source_1 = color_frame_1, color_image_1
-        mapped_frame_2, color_source_2 = color_frame_2, color_image_2
+    # Camera 2
+    # Wait for a coherent pair of frames: depth and color
+    frames_2 = pipeline_2.wait_for_frames()
+    depth_frame_2 = frames_2.get_depth_frame()
+    # color_frame_2 = frames_2.get_color_frame()
 
-        points_1 = pc_1.calculate(depth_frame_1)
-        points_2 = pc_2.calculate(depth_frame_2)
+    #NOTE This is what reduces the pointcloud density.
+    depth_frame_1 = decimate.process(depth_frame_1)
+    depth_frame_2 = decimate.process(depth_frame_2)
 
-        pc_1.map_to(mapped_frame_1)
-        pc_2.map_to(mapped_frame_2)
 
-        return points_1, points_2, mapped_frame_1, mapped_frame_2
+    # for f in filters:
+    #         depth_frame_1 = f.process(depth_frame_1)
+    #         depth_frame_2 = f.process(depth_frame_2)
+
+    # Convert images to numpy arrays
+    depth_image_2 = np.asanyarray(depth_frame_2.get_data())
+    color_image_2 = np.asanyarray(color_frame_2.get_data())
+    colorized_depth_2 = colorizer_2.colorize(depth_frame_2)
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap_2 = np.asanyarray(colorized_depth_2.get_data())
+    depth_colormap_2 = cv2.applyColorMap(cv2.convertScaleAbs(depth_image_2, alpha=0.5), cv2.COLORMAP_JET)
+    # Stack all images horizontally
+    mapped_frame_1, color_source_1 = color_frame_1, color_image_1
+    mapped_frame_2, color_source_2 = color_frame_2, color_image_2
+
+    points_1 = pc_1.calculate(depth_frame_1)
+    points_2 = pc_2.calculate(depth_frame_2)
+
+    pc_1.map_to(mapped_frame_1)
+    pc_2.map_to(mapped_frame_2)
+
+    return points_1, points_2, mapped_frame_1, mapped_frame_2
 
 
 def save(event,x,y,flags,param):
+    """
+    This function is designed to work with CV2 callback. 
+
+    Double left click saves and displays the pointcloud.
+
+    Left click only saves. If held, it shoud continuously save data.
+    """
+    global save_index
+    # check if it was double click first, if so, save and display.
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        global save_index
         save_index+=1
-        points_1, points_2, mapped_frame_1, mapped_frame_2 = get_depth_data(param[0],param[1])
-        # points_2, mapped_frame_2 = get_depth_data(param[1])
+        points_1, points_2, mapped_frame_1, mapped_frame_2 = get_depth_data(param[0],param[1], param[2], param[3])
         print((save_path + "816612061111_no"+str(save_index)+ ".ply"))
         print((save_path + "816612061344_no"+str(save_index)+ ".ply"))
         points_1.export_to_ply((save_path + "816612061111_no"+str(save_index)+".ply"),mapped_frame_1)
         points_2.export_to_ply((save_path + "816612061344_no"+str(save_index)+".ply"),mapped_frame_2)
 
-        # o3d_view_pointcloud((save_path + "816612061111_no"+str(save_index)+".ply"),
-        #                     (save_path + "816612061344_no"+str(save_index)+".ply"))
+        o3d_view_pointcloud((save_path + "816612061111_no"+str(save_index)+".ply"),
+                            (save_path + "816612061344_no"+str(save_index)+".ply"))
 
+        # view_pointcloud((save_path + "816612061111_no"+str(save_index)+".ply"),
+        #                 (save_path + "816612061344_no"+str(save_index)+".ply"))
 
-        view_pointcloud((save_path + "816612061111_no"+str(save_index)+".ply"),
-                        (save_path + "816612061344_no"+str(save_index)+".ply"))
+    # Otherwise check and see if left button is down (no double click) and simply save.
+    # If left click is held down, it shoud record continuously
+    elif event == cv2.EVENT_LBUTTONDOWN:
+        save_index+=1
+        points_1, points_2, mapped_frame_1, mapped_frame_2 = get_depth_data(param[0],param[1], param[2], param[3])
+        print((save_path + "816612061111_no"+str(save_index)+ ".ply"))
+        print((save_path + "816612061344_no"+str(save_index)+ ".ply"))
+        points_1.export_to_ply((save_path + "816612061111_no"+str(save_index)+".ply"),mapped_frame_1)
+        points_2.export_to_ply((save_path + "816612061344_no"+str(save_index)+".ply"),mapped_frame_2)
 
-       
-        print("Saved")
+    print("Saved")
 
+# name for the trackbar. This also acts as the toggle variable.
 switch = '0 : OFF \n1 : ON'
 
 
-
+#create the cv2 window for display then make it fullscreen
 cv2.namedWindow('RealSense', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('RealSense', 1000,480)
+cv2.setWindowProperty('Realsense', cv2.WND_PROP_FULLSCREEN, cv2.CV_WINDOW_FULLSCREEN)
+# cv2.resizeWindow('RealSense', 1000,480)
 
 cv2.createTrackbar(switch, 'RealSense',0,1, nothing)
 
@@ -160,37 +186,40 @@ try:
     print("Preforming initial Check...")
     frames_1 = pipeline_1.wait_for_frames()
     frames_2 = pipeline_2.wait_for_frames()
-    get_depth_data(frames_1, frames_2)
+    color_frame_1 = frames_1.get_color_frame()
+    color_frame_2 = frames_2.get_color_frame()
+    get_depth_data(frames_1, frames_2, color_frame_1, color_frame_2)
     print("Initial test complete...")
 
-    while cv2.getWindowProperty('RealSense', 1) >=0 :
+    # Continuously display the colour frames from the RGB camera on the D415 cameras.
+    while cv2.getWindowProperty('RealSense', 1) >= 0 :
 
         #collect and process only the colour frames for viewing
         frames_1 = pipeline_1.wait_for_frames()
-
         color_frame_1 = frames_1.get_color_frame()
-
         color_image_1 = np.asanyarray(color_frame_1.get_data())
    
         frames_2 = pipeline_2.wait_for_frames()
-
         color_frame_2 = frames_2.get_color_frame()
-
         color_image_2 = np.asanyarray(color_frame_2.get_data())
 
+        #needed for a break for viewing
         cv2.waitKey(1)
 
+        #colour images prepped to display throu CV2
         color_image_1 = cv2.cvtColor(color_image_1, cv2.COLOR_BGR2RGB)
         color_image_2 = cv2.cvtColor(color_image_2, cv2.COLOR_BGR2RGB)
         images = np.hstack((color_image_1,color_image_2))
 
-        # Show images from both cameras
+        #Use the trackbar existance to check if the X has been selected. Quits the program.
         if(cv2.getTrackbarPos(switch,'RealSense')) == -1:
             break
         
+        #display the images
         cv2.imshow('RealSense', images)
         
-        cv2.setMouseCallback('RealSense', save, [frames_1, frames_2])
+        #using CV2 callback, save the images
+        cv2.setMouseCallback('RealSense', save, [frames_1, frames_2, color_frame_1, color_frame_2])
        
         s = cv2.getTrackbarPos(switch,'RealSense')
 
@@ -199,11 +228,10 @@ try:
             save_index += 1
             print((save_path + "816612061111_no"+str(save_index)+ ".ply"))
             print((save_path + "816612061344_no"+str(save_index)+ ".ply"))
-            points_1, points_2, mapped_frame_1, mapped_frame_2 = get_depth_data(frames_1, frames_2)
+            points_1, points_2, mapped_frame_1, mapped_frame_2 = get_depth_data(frames_1, frames_2, color_frame_1, color_frame_2)
 
             points_1.export_to_ply((save_path + "816612061111_no"+str(save_index)+ ".ply"), mapped_frame_1)
             points_2.export_to_ply((save_path + "816612061344_no"+str(save_index)+ ".ply"), mapped_frame_2)
-            # view_pointcloud((save_path + "816612061344_no"+str(save_index)+ ".ply"),(save_path + "816612061344_no"+str(save_index)+ ".ply"))
             print ("Save")
 
 
